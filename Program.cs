5a56aa94721c7fd1733359ee7e4f78c95c24fbca
Antti Nihtilä <68847740+anttinihtila@users.noreply.github.com>
2020-12-29 02:21:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+public class Hello : Form
+{
+    public Hello()
+    {
+        this.Paint += new PaintEventHandler(f1_paint);
+        this.Size = new Size(400, 200);
+    }
+
+    private void f1_paint(object sender, PaintEventArgs e)
+    {
+
+        // Get Graphics Object
+        Graphics g = e.Graphics;
+
+        // Method under System.Drawing.Graphics
+        g.DrawString("Hello, World!", new Font("Verdana", 20),
+        new SolidBrush(Color.Green), 40, 40);
+    }
+
+    public static void Main()
+    {
+        Application.Run(new Hello());
+    }
+
+}
